d8cd46bdd38c59252609c63e3342bd54e5279379
Lineked <lineked@yandex.ru>
2024-11-18 20:52:58 +0300

    Add database integration and 7tv emote handling

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,26 +1,48 @@
 package org.rieg
 
+import com.github.philippheuer.events4j.simple.SimpleEventHandler
 import com.github.twitch4j.TwitchClientBuilder
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.Json
+import database.repository.channels.ChannelsRepository
+import database.repository.channels.ChannelsRepositoryImpl
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.runBlocking
+import org.rieg.database.connectDatabase
+import org.rieg.database.repository.sevenTvEmotes.SevenTvEmotesRepository
+import org.rieg.database.repository.sevenTvEmotes.SevenTvEmotesRepositoryImpl
+import org.rieg.features.SevenTvEmotesHandler
+import org.rieg.sevenTvClient.SevenTvClient
+import org.rieg.sevenTvClient.getEmotes
+import org.rieg.sevenTvClient.model.Emote
 
+private val logger = KotlinLogging.logger {}
 
 fun main() {
-    val client = HttpClient(CIO) {
-        install(ContentNegotiation) {
-            json(
-                Json {
-                    ignoreUnknownKeys = true
-                }
-            )
-        }
-    }
+    connectDatabase()
+    val channelsRepository: ChannelsRepository = ChannelsRepositoryImpl()
+    val sevenTvEmotesRepository: SevenTvEmotesRepository = SevenTvEmotesRepositoryImpl()
     val twitchClient = TwitchClientBuilder.builder()
         .withEnableHelix(true)
+        .withClientId(System.getenv("TWITCH_CLIENT_ID"))
+        .withClientSecret(System.getenv("TWITCH_CLIENT_SECRET"))
+        .withDefaultEventHandler(SimpleEventHandler::class.java)
         .withEnableChat(true)
         .build()
+
+    val sevenTvClient = SevenTvClient()
+    val twitchChannels = channelsRepository.getChannels()
+    val twitchChannelsToEmotes = mutableMapOf<String, Map<String, Emote>>()
+    for (twitchChannel in twitchChannels) {
+        val twitchUsername = twitchChannel.twitchUsername.lowercase()
+        twitchClient.chat.joinChannel(twitchUsername)
+        runBlocking {
+            twitchChannelsToEmotes[twitchUsername] = sevenTvClient.getEmotes(twitchChannel.twitchId)
+        }
+        logger.info {
+            "$twitchUsername - ${twitchChannelsToEmotes[twitchUsername]}"
+        }
+    }
+
+    val eventManager = twitchClient.eventManager.getEventHandler(SimpleEventHandler::class.java)
+    SevenTvEmotesHandler(eventManager, twitchChannelsToEmotes, sevenTvEmotesRepository)
 }
 
